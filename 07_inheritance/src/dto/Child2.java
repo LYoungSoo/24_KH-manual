79d1e4d6a7ea5ce0a2932efd4e5763509363de49
LYoungSoo <108500814+LYoungSoo@users.noreply.github.com>
2024-07-29 17:39:27 +0900

    Delete 07_inheritance/src directory

--- 07_inheritance/src/dto/Child2.java
+++
@@ -1,9 +0,0 @@
-package dto;
-
-public class Child2 extends Parent {
-    
-    @Override
-    public String toString () {
-        return "[Child22]" + super.toString();
-    }
-}
--- 07_inheritance/src/dto/Child3.java
+++
@@ -1,9 +0,0 @@
-package dto;
-
-public class Child3 extends Parent {
-    
-    @Override
-    public String toString () {
-        return "[Child333]" + super.toString();
-    }
-}
